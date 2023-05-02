b545e6b980eded0900adee2bdc5de6c2796ae179
Vyacheslav <kirillov.com@gmail.com>
2023-05-02 22:19:15 +0300

    Initial commit

--- src/main/java/MyApp/MyApp.java
+++ src/main/java/MyApp/MyApp.java
@@ -1,33 +1,47 @@
 package MyApp;
 
+import java.util.Arrays;
+
 public class MyApp {
 
-    public static void main(String[] args) {
-/*
-*       50
-*   40       60
-* 30  45   55   70
-* */
 
 
-
-
+    public void main(String[] args) {
     }
 
-    static class Node {
-        final int key;
-        Node left, right;
-
-        Node(int item) {
-            key = item;
-            left = right = null;
+    public int[] arrayAfterZero(int[] arr){
+        int [] result= new int[]{arr.length};
+        int write=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                write=1;
+                result= Arrays.copyOfRange(arr,i+1,arr.length);
+            }
         }
+        if (write==0){
+            throw  new RuntimeException("No zero founds");
+        }
+        return result;
     }
 
-    public static Node search(int key, Node root) {
-        if (root == null) return null;
-        if (key == root.key) return root;
-        return search(key, key > root.key ? root.right : root.left);
+    public boolean sixNines(int[] arr){
+
+        int sixies=0;
+        int nines=0;
+
+        for (int j : arr) {
+
+            if (j == 66) {
+                sixies = 1;
+            }
+            else if(j==99) {
+                nines = 1;
+            }
+            else
+                return false;
+        }
+
+        return sixies == 1 && nines == 1;
     }
 
 
