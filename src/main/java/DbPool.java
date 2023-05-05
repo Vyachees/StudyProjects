f986777f940b22ae4cdbea87c66689e797de5a77
Vyacheslav <kirillov.com@gmail.com>
2023-05-05 20:03:00 +0300

    Initial2

--- src/main/java/DbPool.java
+++ src/main/java/DbPool.java
@@ -5,22 +5,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DbPool {
-/*
-* Что нужно сделать:
-Написать на java программу для подключения к базе данных H2,
-используя данные (заполненные таблицы) из уроков 3 модуля
-Например: перебрать всех клиентов в таблице клиент
-
-Что должно получиться в результате:
-Подключение и установка соединения с H2
-Вывод на экран содержимого в таблице client
-добавление/изменение записи в таблице client
-фиксирование изменений в транзакции
-*
-* Дополнительные материалы:
-Установка соединения и работа с БД через connection pool
-* */
-
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
 
